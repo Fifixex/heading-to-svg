0952509572b57a307dcab3df6b759020267443c5
Fifixex <122852085+Fifixex@users.noreply.github.com>
2025-02-12 08:56:36 +0000

    :recycle: refactor(api): change route name to index

--- api/index.ts
+++ api/index.ts
@@ -7,13 +7,13 @@ export const config = {
 
 const app = new Hono().basePath('/api')
 
-app.get('/text', (c) => {
-    const { content, width = 100, height = 80 } = c.req.query()
+app.get('/', (c) => {
+    const { text, width = 100, height = 80 } = c.req.query()
 
     c.header('Content-Type', 'image/svg+xml')
     return c.body(`
     <svg xmlns="http://www.w3.org/2000/svg" width="${width}px" height="${height}px">
-        <text x="10" y="20" style="font-size: 24px">${content}</text>
+        <text x="10" y="20" style="font-size: 24px">${text}</text>
     </svg>
     `)
 })
